d3e1b16960642e6671957417ab840d6a2388b190
m0l1n <quentinmollardgre@gmail.com>
2014-04-08 11:13:50 +0200

    ajout des database et remove des accesBD

--- src/database/BDConnexion.java
+++ src/database/BDConnexion.java
@@ -1,36 +1,29 @@
 package database;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 
 import jus.util.IO;
+
+import oracle.jdbc.pool.OracleDataSource;
 import exceptions.ExceptionConnexion;
 
 public final class BDConnexion {
 
-	public BDConnexion() {
+	public BDConnexion () {
 
 	}
-
 	/**
-	 * Obtenir une nouvelle connexion a la BD, en fonction des parametres
-	 * contenus dans un fichier de configuration.
-	 * 
-	 * @return une nouvelle connexion a la BD
-	 * @throws ExceptionConnexion
-	 *             si la connexion a echoue
-	 */
+	    * Obtenir une nouvelle connexion a la BD, en fonction des parametres
+	    * contenus dans un fichier de configuration.
+	    * @return  une nouvelle connexion a la BD
+	    * @throws ExceptionConnexion si la connexion a echoue
+	    */
 
-	public static Connection getConnexion()
-			throws ExceptionConnexion {
-		Connection conn = null;
+	public static Connection getConnexion() throws ExceptionConnexion {
+		Connection conn = null ;
 		try {
 
 			// lecture des parametres de connexion dans connection.conf
@@ -39,43 +32,35 @@ public final class BDConnexion {
 			is = new FileInputStream(utils.Constantes.Config);
 			p.load(is);
 			String url = p.getProperty("url");
-			String driver = p.getProperty("driver");
 			String login = p.getProperty("user");
 			String mdp = p.getProperty("mdp");
+			String driver = p.getProperty("driver");
 			
 			Class.forName(driver);
 			// hopper@UFR, Oracle
-			conn = DriverManager.getConnection(url, login, mdp);
+			conn = DriverManager.getConnection(url,login,mdp);
 			conn.setAutoCommit(false);
 		} catch (SQLException e) {
-			IO.afficherln("Connexion impossible : " + e.getMessage());// handle
-																		// any
-																		// errors
+			IO.afficherln("Connexion impossible : " + e.getMessage());// handle any errors
 			IO.afficherln("SQLException: " + e.getMessage());
 			IO.afficherln("SQLState: " + e.getSQLState());
 			IO.afficherln("VendorError: " + e.getErrorCode());
 		} catch (IOException e) {
-			throw new ExceptionConnexion("fichier conf illisible \n"
-					+ e.getMessage());
+			throw new ExceptionConnexion ("fichier conf illisible \n" + e.getMessage());
 		} catch (ClassNotFoundException e) {
-			throw new ExceptionConnexion(
-					"problème d'identification du pilote \n" + e.getMessage());
+			throw new ExceptionConnexion ("problème d'identification du pilote \n" + e.getMessage());
 		}
-		return conn;
+		return conn ;
 	}
 
 	/**
-	 * Fermer la connexion, l'instruction et la structure de resultats. Fermer
-	 * les 3 a la fois semble correspondre a de nombreux cas.
-	 * 
-	 * @param conn
-	 *            la connexion
-	 * @param stmt
-	 *            l'instruction
-	 * @param rs
-	 *            la structure de resultats
+	 * Fermer la connexion, l'instruction et la structure de resultats. Fermer les
+	 * 3 a la fois semble correspondre a de nombreux cas.
+	 * @param conn la connexion
+	 * @param stmt l'instruction
+	 * @param rs la structure de resultats
 	 */
-	public static void FermerTout(Connection conn, Statement stmt, ResultSet rs) {
+	public static void FermerTout (Connection conn, Statement stmt, ResultSet rs){
 		if (rs != null) {
 			try {
 				rs.close();
@@ -87,7 +72,8 @@ public final class BDConnexion {
 		if (stmt != null) {
 			try {
 				stmt.close();
-			} catch (SQLException e) {
+			}
+			catch (SQLException e) {
 				;
 			}
 			stmt = null;
